731ea2032d3bf91dcc302b8d51137e7350dadbc0
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-08-02 00:11:36 +0430

    add clearBtn function and numbers eventListeners handeled

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector(".clear");
 
+let temp;
+clearBtn.addEventListener("click", () => {
+  input.value = "";
+  temp = "";
+});
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.textContent == "C") return;
+    input.value = input.value + e.target.textContent;
+    temp = input.value;
+  });
+});
