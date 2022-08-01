5c248978e93c4734c55457dcdc271c8a900e1617
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-08-01 17:33:53 +0430

    get elements from DOM

---
+++ main.js
@@ -0,0 +1,5 @@
+// get element from DOM
+const input = document.querySelector(".form-input");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
